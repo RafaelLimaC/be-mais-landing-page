841ba723b50af716d73bdac3125ae3e8c418ccb3
Rafael Lima <srafalimacosta@gmail.com>
2023-09-13 18:24:35 -0300

    <feat> animation on scroll

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
 const chk = document.getElementById('chk');
+const logo = document.getElementById('logo')
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark');
+}
+
+toggleDarkMode()
+toggleLogo()
+
+function toggleLogo() {
+    if (logo.getAttribute('src') == "/assets/logo-header.svg") {
+      logo.setAttribute('src', "/assets/logo-header-dark.svg");
+    } else {
+      logo.setAttribute('src', "/assets/logo-header.svg");
+    }
 }
 
 // toggleDarkMode()
 
 chk.addEventListener('change', () => {
 	toggleDarkMode();
+    toggleLogo();
 });
+
+const hiddenElements = document.querySelectorAll('.hidden')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+hiddenElements.forEach((el) => observer.observe(el));
