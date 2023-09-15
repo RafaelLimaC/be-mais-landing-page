c7c34c69dcafb6e87da41dc9a6016d1d9409e9df
Rafael Lima <srafalimacosta@gmail.com>
2023-09-15 15:36:35 -0300

    <feat> testimonial slider

--- js/main.js
+++ js/main.js
@@ -80,3 +80,19 @@ btnDialog.forEach(button => {
         dialog.showModal();
     });
 });
+
+// swiper 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
